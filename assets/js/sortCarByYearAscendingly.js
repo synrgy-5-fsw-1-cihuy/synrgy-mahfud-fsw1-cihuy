91f698fba5ec48aa31b2538964462282c3270f8c
Ilyas Mahfud <ilyasmahfud39@gmail.com>
2022-10-12 19:14:20 +0700

    init challenge 3

---
+++ assets/js/sortCarByYearAscendingly.js
@@ -0,0 +1,14 @@
+/**
+ Disini kamu akan membuat sebuah fungsi yang berguna untuk mensortir daftar mobil,
+ dimana setelah fungsi ini dijalankan maka daftar mobil yang telah diurutkan dari 
+ umur mobil paling tua akan menjadi hasil dari fungsi tersebut.
+**/
+
+function sortCarByYearAscendingly(cars) {
+    // Tulis code-mu disini
+    const result = [];
+
+    return result; 
+}
+
+sortCarByYearAscendingly(cars);
